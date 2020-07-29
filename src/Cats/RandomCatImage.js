dc65c7d65c1a6241fe75479d6bea787aa5a75aae
Saulet Yskak <sauletik01@gmail.com>
2020-07-29 11:45:44 +0600

    Homework 2: Step 1

--- src/Cats/RandomCatImage.js
+++ src/Cats/RandomCatImage.js
@@ -2,36 +2,62 @@ import React from "react";
 import styled from "styled-components";
 
 const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
 const Image = styled.img`
-  width: 100%;
+    width: 100%;
 `;
 
-// https://docs.thecatapi.com/
-
 function RandomCatImage(props) {
-  const [catImageUrl, setCatImageUrl] = React.useState(null);
+    const [catImageUrl, setCatImageUrl] = React.useState(null);
+    const [countDownTime, setCountDown] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setCatImageUrl(data[0].url);
-      });
-  }, []);
+    // *******************
+    // Adapted from: https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
+    // *******************
+    const countDown = () => {
+        const duration = 30;
+        var timer = duration,
+            minutes,
+            seconds;
 
-  if (catImageUrl == null) return <div> Loading </div>;
+        setInterval(function() {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
 
-  return (
-    <MainContainer>
-      <Image src={catImageUrl} />
-    </MainContainer>
-  );
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            setCountDown(`${minutes}: ${seconds}`);
+
+            if (--timer < 0) {
+                fetchData();
+                timer = duration;
+            }
+        }, 1000);
+    };
+
+    async function fetchData() {
+        const res = await fetch("https://api.thecatapi.com/v1/images/search");
+        res.json().then(res => setCatImageUrl(res[0].url));
+    }
+
+    React.useEffect(() => {
+        countDown();
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    if (catImageUrl == null) return <div> Loading </div>;
+
+    return (
+        <MainContainer>
+            <Image src={catImageUrl} />
+            <h2>New cat image will appear in {countDownTime} seconds</h2>
+        </MainContainer>
+    );
 }
 
 export default RandomCatImage;
